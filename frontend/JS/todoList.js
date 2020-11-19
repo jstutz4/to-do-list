0b73cd56a3ace3fdcad8d4797d4ffb84f5672844
jstutz4 <stu15012@gmail.com>
2020-11-19 14:24:48 -0800

    test node.js

---
+++ frontend/JS/todoList.js
@@ -0,0 +1,36 @@
+var statusColors = ["complete", "inprogress", "notstarted"]
+function statusToggle(circle)
+{
+   var strClasses =  circle.className;
+   statusSize = statusColors.length;
+   if(strClasses.search("status-inprogress") != -1)
+   {
+      strClasses = strClasses.replace("status-inprogress", "status-complete");
+    //   make a call to update status
+   }
+   else if(strClasses.search("status-notstarted") != -1)
+   {
+    strClasses = strClasses.replace("status-notstarted", "status-inprogress");
+    //   make a call to update status
+   }
+   circle.className = strClasses;
+   
+}
+
+function creatTask()
+{
+    console.log("add a skeleton or modal and regenerate list")
+}
+
+function editTask(element)
+{
+    console.log("add modal insert new data");
+   var itemId = element.parentElement.parentElement.id;
+    document.getElementById(itemId).children[1].children[0].textContent;
+}
+
+function deleteTask(element)
+{
+    console.log("remove task display none");
+    element.parentElement.parentElement.style.display = "none";
+}
---
+++ todoList.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const url = require("url");
+const fs = require("fs");
+const {
+    Pool
+} = require("pg");
+const PORT = process.env.PORT || 8100;
+const app = express();
+'use strict';
+const request = require('request');
+var stream;
+const connnectionString = process.env.DATABASE_URL || "postgres://stockdev:stockdev@localhost:5432/stockmanger";
+const pool = new Pool({
+    connectionString: connnectionString
+});
+
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+app.use(express.static("frontend"));
+app.get('/', (req, res) => res.render("home"))
