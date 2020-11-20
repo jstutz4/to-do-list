aa343bcd971e8c7bfee3165e01f2718ef539e98f
jstutz4 <stu15012@gmail.com>
2020-11-20 12:18:05 -0800

    insert into db

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+
+function addToDoItem(request, response)
+{
+  var sql = ("INSERT INTO todolist (title, status) VALUES( $1::text, $2::int)");
+
+  var params = [request.query]
+  console.log("query start")
+  console.log(request.query)
+  console.log(url.parse(req.url,true).query)
+  console.log("query end")
+
+  
+
+}
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,4 +30,5 @@ express()
   .set('view engine', 'ejs')
   .use(express.static("public/html"))
   .get('/', (req, res) => res.render("pages/index"))
+  .get('/add', addToDoItem)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
---
+++ public/js/dbcalls.js
@@ -0,0 +1,15 @@
+function addTask()
+{
+    var url = '/add?title=add to the db via user request&description=';
+    console.log('url:  ' + url);
+    var httpRequest = new XMLHttpRequest();
+
+    httpRequest.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(this.responseText);
+            console.log(obj);
+        }
+    }
+    httpRequest.open("GET", url, true);
+    httpRequest.send();
+}
