9079456481bf2947ada9840d02063d4c66735f11
jstutz4 <stu15012@gmail.com>
2020-11-20 15:41:34 -0800

    read db display list

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function getAllFromDB(request, response)
   pool.query(sql, (error, result)=>{
     if(!error)
     {
-      response.render('partials/todolist_items', JSON.stringify(result.rows))
+      response.render('partials/todolist_items', {data: JSON.stringify(result.rows)})
     }
     else
     {
