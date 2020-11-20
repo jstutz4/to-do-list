c5d70430f898869e7b63581bb7e15a4aefdeb8ae
jstutz4 <stu15012@gmail.com>
2020-11-20 13:05:28 -0800

    insert into db

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ function addToDoItem(request, response)
     console.log(result)
     if (!error)
     {
-      response.render("JSON", {success: true})
+      response.json({success: true})
     }
     else
     {
-      response.render("JSON", {success: false})
+      response.json({success: false})
 
     }
   });
