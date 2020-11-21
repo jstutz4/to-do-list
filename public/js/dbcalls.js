b858309d6ad3ceda7ca0e2913587498b49ad7d18
jstutz4 <stu15012@gmail.com>
2020-11-20 16:55:04 -0800

    read db display list

--- public/js/dbcalls.js
+++ public/js/dbcalls.js
@@ -26,9 +26,11 @@ function getTodolist()
 
     httpRequest.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            var data = JSON.parse(this.responseText[0]).data;
-            console.log("see date here")
-            console.log(data)
+            var newdata = this.responseText;
+            console.log(newdata);
+            var data = JSON.parse(this.responseText).data;
+            console.log("see date here");
+            console.log(data);
             
             document.getElementById('todolist_container').innerHTML = displayTodoList(data);
         }
